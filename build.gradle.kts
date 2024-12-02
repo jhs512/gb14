abdebe73441ef79cafc9cf1b3b71e403b78eacab
jhs512 <jangka512@gmail.com>
2024-12-03 01:04:48 +0900

    postService, b

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,15 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    compileOnly("org.projectlombok:lombok:1.18.36")
+    annotationProcessor("org.projectlombok:lombok:1.18.36")
+
+    testCompileOnly("org.projectlombok:lombok:1.18.36")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.36")
+
+    implementation("org.reflections:reflections:0.10.2")
+    implementation("ch.qos.logback:logback-classic:1.5.12")
 }
 
 tasks.test {
--- src/main/java/com/ll/spring/ApplicationContext.java
+++ src/main/java/com/ll/spring/ApplicationContext.java
@@ -1,10 +1,14 @@
 package com.ll.spring;
 
-import com.ll.domain.post.post.controller.ApiV1PostController.ApiV1PostController;
-import com.ll.domain.post.post.service.PostService;
-
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+
+import org.reflections.Reflections;
+import org.reflections.scanners.Scanners;
+
+import com.ll.spring.annotation.RestController;
+import com.ll.spring.annotation.Service;
 
 public class ApplicationContext {
     private final Map<String, Object> beans = new HashMap<>();
@@ -14,8 +18,27 @@ public class ApplicationContext {
     }
 
     private void scan(String basePackage) {
-        beans.put("apiV1PostController", new ApiV1PostController());
-        beans.put("postService", new PostService());
+        Reflections reflections = new Reflections(basePackage, Scanners.TypesAnnotated);
+
+        // Component로 마크된 실제 구현 클래스들만 스캔
+        Set<Class<?>> components = reflections.getTypesAnnotatedWith(RestController.class, true);
+
+        // 스테레오타입 어노테이션들로 마크된 클래스들도 추가
+        components.addAll(reflections.getTypesAnnotatedWith(Service.class, true));
+
+        // 스캔된 컴포넌트들을 객체화하여 beans 맵에 저장
+        for (Class<?> componentClass : components) {
+            try {
+                String beanName = componentClass.getSimpleName();
+                // 첫 글자를 소문자로 변환하여 빈 이름 생성
+                beanName = Character.toLowerCase(beanName.charAt(0)) + beanName.substring(1);
+                
+                Object instance = componentClass.getDeclaredConstructor().newInstance();
+                beans.put(beanName, instance);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     public <T> T getBean(String beanName) {
