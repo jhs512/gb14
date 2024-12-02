ab76b647d572c97f5ca3cf893718bee2d9b1eb7a
jhs512 <jangka512@gmail.com>
2024-12-03 01:24:26 +0900

    postService, b

--- src/main/java/com/ll/spring/ApplicationContext.java
+++ src/main/java/com/ll/spring/ApplicationContext.java
@@ -3,15 +3,15 @@ package com.ll.spring;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Parameter;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.reflections.Reflections;
 import org.reflections.scanners.Scanners;
 
 import com.ll.spring.annotation.Component;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 public class ApplicationContext {
     private final Map<String, Object> beans = new HashMap<>();
@@ -46,10 +46,70 @@ public class ApplicationContext {
         for (Class<?> candidate : candidates) {
             if (isConcreteClass(candidate)) {
                 String beanName = generateBeanName(candidate);
-                Object instance = instantiateClass(candidate);
-                registerBean(beanName, instance);
+                createBean(candidate);
             }
         }
+    }
+
+    private Object createBean(Class<?> cls) {
+        String beanName = generateBeanName(cls);
+        
+        // 이미 생성된 빈이 있다면 반환
+        Object existingBean = beans.get(beanName);
+        if (existingBean != null) {
+            return existingBean;
+        }
+
+        // 생성 진행 중임을 표시 (순환 참조 감지용)
+        beans.put(beanName, null);
+
+        try {
+            Constructor<?> constructor = cls.getDeclaredConstructors()[0];
+            Object[] params = new Object[constructor.getParameterCount()];
+            
+            // 생성자 파라미터의 의존성을 먼저 해결
+            for (int i = 0; i < constructor.getParameterCount(); i++) {
+                Parameter parameter = constructor.getParameters()[i];
+                Class<?> parameterType = parameter.getType();
+                
+                // 의존성 빈을 찾거나 생성
+                Object dependency = findBeanByType(parameterType);
+                if (dependency == null) {
+                    // 의존성 빈이 없다면 해당 타입의 빈을 찾아서 생성
+                    dependency = createBeanOfType(parameterType);
+                }
+                
+                if (dependency == null) {
+                    throw new RuntimeException(
+                        String.format("No bean found for parameter type: %s in class: %s", 
+                            parameterType.getName(), 
+                            cls.getName()
+                        )
+                    );
+                }
+                
+                params[i] = dependency;
+            }
+
+            // 모든 의존성이 해결되면 인스턴스 생성
+            Object instance = constructor.newInstance(params);
+            beans.put(beanName, instance);
+            return instance;
+            
+        } catch (Exception e) {
+            beans.remove(beanName); // 실패시 생성 진행 중 표시 제거
+            throw new RuntimeException("Failed to create bean: " + cls.getName(), e);
+        }
+    }
+
+    private Object createBeanOfType(Class<?> type) {
+        // 해당 타입의 구현체를 찾아서 생성
+        for (Class<?> candidate : scanComponents()) {
+            if (isConcreteClass(candidate) && type.isAssignableFrom(candidate)) {
+                return createBean(candidate);
+            }
+        }
+        return null;
     }
 
     private boolean isConcreteClass(Class<?> cls) {
@@ -59,36 +119,6 @@ public class ApplicationContext {
     private String generateBeanName(Class<?> cls) {
         String className = cls.getSimpleName();
         return Character.toLowerCase(className.charAt(0)) + className.substring(1);
-    }
-
-    private Object instantiateClass(Class<?> cls) {
-        try {
-            Constructor<?> constructor = cls.getDeclaredConstructors()[0]; // RequiredArgsConstructor로 생성된 생성자 사용
-            
-            if (constructor.getParameterCount() == 0) {
-                return constructor.newInstance();
-            }
-
-            // 생성자 파라미터에 맞는 빈들을 찾아서 주입
-            List<Object> params = new ArrayList<>();
-            for (Parameter parameter : constructor.getParameters()) {
-                Class<?> parameterType = parameter.getType();
-                Object dependency = findBeanByType(parameterType);
-                if (dependency == null) {
-                    throw new RuntimeException(
-                        String.format("No bean found for parameter type: %s in class: %s", 
-                            parameterType.getName(), 
-                            cls.getName()
-                        )
-                    );
-                }
-                params.add(dependency);
-            }
-
-            return constructor.newInstance(params.toArray());
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to instantiate class: " + cls.getName(), e);
-        }
     }
 
     private Object findBeanByType(Class<?> type) {
